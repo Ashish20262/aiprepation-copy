b6aa72c01acac63fdc7b9442d31121d09a4df6a1
Ashish prajapati <ap5009010@gmail.com>
2026-06-10 12:04:49 +0530

    Change API base URL to production environment

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3500",
+    baseURL: "https://aiprepation-copy-1.onrender.com",
     withCredentials: true
 })
 
